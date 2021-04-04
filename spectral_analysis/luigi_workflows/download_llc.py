5e2aacce3ca91e3080d2c3771ff4adec1010fe42
Antonio <cesarperez@cicese.edu.mx>
2021-04-04 06:05:01 -0700

    Omit Eta from Luigi download

--- spectral_analysis/luigi_workflows/download_llc.py
+++ spectral_analysis/luigi_workflows/download_llc.py
@@ -37,7 +37,7 @@ ds_path_fmt = LUIGI_OUT_FOLDER + "/Datasets_compressed/{}/{}"
 Omega = 2*np.pi/(24*3600) # Frecuencia de rotación terrestre
 #vars_wf = ["U","V"] # Variables del modelo
 #vars_wf = ["U","V","oceQnet","oceTAUX","oceTAUY"]
-vars_wf = ["KPPhbl","oceQnet","U","V","oceQnet","oceTAUX","oceTAUY","Theta","Eta"]
+vars_wf = ["KPPhbl","oceQnet","U","V","oceQnet","oceTAUX","oceTAUY","Theta"]
 #vars_wf = ["Eta"]
 #k_lvl_idx = [0, 36]  ## z=0 m, z=-400 m (39 para z=-500 m)
 #k_lvl_idx = [0,6,12,16,19]#,21,25 ## Para T, y quizás S
