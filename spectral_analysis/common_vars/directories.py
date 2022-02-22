4d45759715a4672e30bbd433e3abe564806f0952
Antonio <cesarperez@cicese.edu.mx>
2022-02-22 09:17:46 -0800

    chore: move to pc-64gb directory

--- spectral_analysis/common_vars/directories.py
+++ spectral_analysis/common_vars/directories.py
@@ -1,6 +1,7 @@
-DATA_FOLDER = "/data/LLC4320/GDriveBackup/Tesis/data"
+LLC4320_BASE_FOLDER="/mnt/Datos4TB/antonio/Tesis/LLC4230"
+DATA_FOLDER = "{}/GDriveBackup/Tesis/data".format(LLC4320_BASE_FOLDER)
 SPECTRA_FOLDER = "{}/spectra".format(DATA_FOLDER)
 MODEL_FOLDER = "{}/model".format(DATA_FOLDER)
 OUT_IMGS_FOLDER = "{}/output_images".format(DATA_FOLDER)
-LUIGI_OUT_FOLDER = "/data/LLC4320/Luigi"
-POSTPROCESS_OUT_FOLDER = "/data/LLC4320/Postprocess"
+LUIGI_OUT_FOLDER = "{}/Luigi".format(LLC4320_BASE_FOLDER)
+POSTPROCESS_OUT_FOLDER = "{}/Postprocess".format(LLC4320_BASE_FOLDER)
